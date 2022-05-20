0d557170d00e9f3909306b4478de47610b3392fd
oreze <kunas1@o2.pl>
2022-05-20 21:52:28 +0200

    Add default offers db context

---
+++ src/Services/Offers/Offers.Infrastructure/Data/OffersDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Offers.Infrastructure.Data;
+
+public class OffersDbContext: DbContext
+{
+    public OffersDbContext(DbContextOptions<OffersDbContext> options) : base(options)
+    {
+        
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+    }
+}
