7d7b9175b5ed1ace04e5455225ffddf3fd93c5e0
oreze <kunas1@o2.pl>
2022-05-22 14:09:55 +0200

    Add LocationValidatior with location validation constants class
    Signed-off-by: oreze <kunas1@o2.pl>

---
+++ src/Services/Offers/Offers.Domain/AggregationModels/Offer/Location/LocationValidationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Offers.Domain.AggregationModels.Offer.Location;
+
+public static class LocationValidationConstants
+{
+    public static int NameMinimumLength = 2;
+    public static int NameMaximumLength = 100;
+    public static int CountryISOCodeLength = 3;
+}
---
+++ src/Services/Offers/Offers.Domain/AggregationModels/Offer/Location/LocationValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Offers.Domain.AggregationModels.Offer.Location;
+
+public class LocationValidator: AbstractValidator<LocationAggregate>
+{
+    public LocationValidator()
+    {
+        RuleFor(x => x.Name)
+            .MinimumLength(LocationValidationConstants.NameMinimumLength)
+            .WithMessage($"Name is too short. It has to be minimum {LocationValidationConstants.NameMinimumLength} chars length.")
+            .MaximumLength(LocationValidationConstants.NameMaximumLength)
+            .WithMessage($"Name is too short. It has to be maximum {LocationValidationConstants.NameMaximumLength} chars length.");
+        
+        RuleFor(x => x.CountryISO)
+            .NotNull()
+            .WithMessage("Country ISO cannot be empty.")
+            .Length(LocationValidationConstants.CountryISOCodeLength)
+            .WithMessage("Country ISO has to be in three characters length.");
+    }
+}
