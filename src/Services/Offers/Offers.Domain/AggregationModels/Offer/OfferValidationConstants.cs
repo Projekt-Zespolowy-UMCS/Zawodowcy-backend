b1a2421690fbff519d4406fe1ebbbb39e7acc663
oreze <kunas1@o2.pl>
2022-05-22 14:31:37 +0200

    Add OfferValidator and offer validation constants class
    Signed-off-by: oreze <kunas1@o2.pl>

---
+++ src/Services/Offers/Offers.Domain/AggregationModels/Offer/OfferValidationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Offers.Domain.AggregationModels.Offer;
+
+public static class OfferValidationConstants
+{
+    public static int TitleMinimumLength = 2;
+    public static int TitleMaximumLength = 100;
+    public static int ContentMinimumLength = 2;
+    public static int ContentMaximumLength = 3000;
+}
