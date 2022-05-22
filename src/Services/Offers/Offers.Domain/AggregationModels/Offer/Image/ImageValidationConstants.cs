4153736394a36d452c166997ade6513a81d6d81a
oreze <kunas1@o2.pl>
2022-05-22 13:38:30 +0200

    Add ImageValidator with validation constants

---
+++ src/Services/Offers/Offers.Domain/AggregationModels/Offer/Image/ImageValidationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Offers.Domain.AggregationModels.Offer.Image;
+
+public class ImageValidationConstants
+{
+    public static int NameMinimumLength = 2;
+    public static int NameMaxinumLength = 100;
+    
+}
