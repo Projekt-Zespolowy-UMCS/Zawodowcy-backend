de0d1b7bf166045b678b3bfe8ce0b3f8618b8200
oreze <kunas1@o2.pl>
2022-05-22 13:19:52 +0200

    Add MimeType validator

---
+++ src/Services/Offers/Offers.Domain/AggregationModels/Offer/Image/MimeType/MimeTypeValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Offers.Domain.AggregationModels.Offer.Image.MimeType;
+
+public class MimeTypeValidator: AbstractValidator<MimeTypeAggregate>
+{
+    public MimeTypeValidator()
+    {
+        RuleFor(x => x.Type)
+            .Matches(@"\w+\/[-+.\w]+")
+            .WithMessage("Type name does not match mime type regex. Is it valid?");
+    }
+}
