2e798c4b42c74cff1a3a82b73150f5d24c30cb71
oreze <kunas1@o2.pl>
2022-05-28 19:46:43 +0200

    Add category entity config
    Signed-off-by: oreze <kunas1@o2.pl>

---
+++ src/Services/Offers/Offers.Infrastructure/EntityConfiguration/CategoryEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Offers.Domain.AggregationModels.Offer.Category;
+
+namespace a;
+
+public class CategoryEntityConfiguration: IEntityTypeConfiguration<CategoryAggregate>
+{
+    public void Configure(EntityTypeBuilder<CategoryAggregate> builder)
+    {
+        builder.ToTable("Category");
+        
+        builder
+            .Property(category => category.Name)
+            .IsRequired(true)
+            .HasMaxLength(CategoryValidationConstants.NameMaximumLength);
+
+
+        // var navigation = builder.Metadata.FindNavigation(nameof(ApplicationUser.Country));
+        // navigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
