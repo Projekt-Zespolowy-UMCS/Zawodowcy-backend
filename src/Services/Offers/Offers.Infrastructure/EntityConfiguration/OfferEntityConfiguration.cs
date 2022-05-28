d1a2c8c612ec2468f8904303e760245e24405f95
oreze <kunas1@o2.pl>
2022-05-28 20:56:49 +0200

    Add offer entity config

---
+++ src/Services/Offers/Offers.Infrastructure/EntityConfiguration/OfferEntityConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Offers.Domain.AggregationModels.Offer;
+using Offers.Domain.AggregationModels.Offer.Image.MimeType;
+
+namespace a;
+
+public class OfferEntityConfiguration: IEntityTypeConfiguration<OfferAggregateRoot>
+{
+    public void Configure(EntityTypeBuilder<OfferAggregateRoot> builder)
+    {
+        builder.ToTable("Offer");
+        
+        ConfigureProperties(builder);
+        ConfigureRelationships(builder);
+    }
+
+    private void ConfigureProperties(EntityTypeBuilder<OfferAggregateRoot> builder)
+    {
+        builder
+            .Property(offer => offer.Content)
+            .IsRequired()
+            .HasMaxLength(OfferValidationConstants.ContentMaximumLength);
+
+        builder
+            .Property(offer => offer.Title)
+            .IsRequired()
+            .HasMaxLength(OfferValidationConstants.TitleMaximumLength);
+    }
+
+    private void ConfigureRelationships(EntityTypeBuilder<OfferAggregateRoot> builder)
+    {
+        builder
+            .HasOne(offer => offer.Category)
+            .WithMany()
+            .HasForeignKey(offer => offer.CategoryId);
+
+        builder
+            .HasOne(offer => offer.Location)
+            .WithMany()
+            .HasForeignKey(offer => offer.LocationId);
+
+        builder
+            .HasMany(offer => offer.Images)
+            .WithOne();
+    }
+}
