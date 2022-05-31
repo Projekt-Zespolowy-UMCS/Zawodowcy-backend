5136501de5b3f532b4a6fb78d38124eadfc6e0b9
oreze <kunas1@o2.pl>
2022-05-31 10:47:39 +0200

    Add ImageRepository
    Fix OfferId in ImageAggregate to be Guid
    Change methods signature to use Guid

---
+++ src/Services/Offers/Offers.Infrastructure/Repositories/ImageRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Offers.Domain.AggregationModels.Offer.Image;
+using Offers.Infrastructure.Data;
+
+namespace Offers.Infrastructure.Repositories;
+
+public class ImageRepository: IImageRepository
+{
+    private readonly OffersDbContext _context;
+
+    public ImageRepository(OffersDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<ImageAggregate> AddImageAsync(ImageAggregate image)
+    {
+        await _context.Images.AddAsync(image);
+        await _context.SaveChangesAsync();
+
+        return image;
+    }
+
+    public async Task<IList<ImageAggregate>> GetOfferImagesAsync(Guid offerId)
+    {
+        return await _context.Images.Where(image => image.OfferId == offerId).ToListAsync();
+    }
+
+    public async Task<ImageAggregate> UpdateImageAsync(ImageAggregate image)
+    {
+        var entity = await _context.Images.FirstAsync(img => img.Id == image.Id);
+
+        _context.Entry(entity).CurrentValues.SetValues(image);
+        await _context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<bool> RemoveImageAsync(Guid id)
+    {
+        try
+        {
+            var entity = await _context.Images.FirstAsync(img => img.Id == id);
+
+            _context.Images.Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+        return true;
+    }
+}
