dba2a2d1be009d179739746e0a365626a94d13ad
oreze <kunas1@o2.pl>
2022-05-31 09:52:50 +0200

    Add repository for MimeType

---
+++ src/Services/Offers/Offers.Infrastructure/Repositories/MimeTypeRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Offers.Domain.AggregationModels.Offer.Image.MimeType;
+using Offers.Infrastructure.Data;
+
+namespace Offers.Infrastructure.Repositories;
+
+public class MimeTypeRepository: IMimeTypeRepository
+{
+    private readonly OffersDbContext _context;
+
+    public MimeTypeRepository(OffersDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<MimeTypeAggregate> AddMimeTypeAsync(MimeTypeAggregate mimeType)
+    {
+        await _context.MimeTypes.AddAsync(mimeType);
+        await _context.SaveChangesAsync();
+
+        return mimeType;
+    }
+
+    public async Task<IList<MimeTypeAggregate>> GetAllAsync()
+    {
+        return await _context.MimeTypes.ToListAsync();
+    }
+}
