682fc650016065ade8eeb3fead561b004aed2256
oreze <kunas1@o2.pl>
2022-05-31 09:43:00 +0200

    Add domain models sets to OffersDbContext

--- src/Services/Offers/Offers.Infrastructure/Data/OffersDbContext.cs
+++ src/Services/Offers/Offers.Infrastructure/Data/OffersDbContext.cs
@@ -1,10 +1,21 @@
 ﻿using a;
 using Microsoft.EntityFrameworkCore;
+using Offers.Domain.AggregationModels.Offer;
+using Offers.Domain.AggregationModels.Offer.Category;
+using Offers.Domain.AggregationModels.Offer.Image;
+using Offers.Domain.AggregationModels.Offer.Image.MimeType;
+using Offers.Domain.AggregationModels.Offer.Location;
 
 namespace Offers.Infrastructure.Data;
 
 public class OffersDbContext: DbContext
 {
+    public DbSet<CategoryAggregate> Categories;
+    public DbSet<MimeTypeAggregate> MimeTypes;
+    public DbSet<LocationAggregate> Locations;
+    public DbSet<ImageAggregate> Images;
+    public DbSet<OfferAggregateRoot> Offers;
+
     public OffersDbContext(DbContextOptions<OffersDbContext> options) : base(options)
     {
         
