a19206ce6b1486d97b03eb8df4b77d1c08e37905
oreze <kunas1@o2.pl>
2022-06-07 20:13:42 +0200

    Add constructor to aggregates
    - create constructor for Category and Offer aggregates
    - refactor entities, remove virtual and change UserId to guid type

--- src/Services/Offers/Offers.Domain/AggregationModels/Offer/Category/CategoryAggregate.cs
+++ src/Services/Offers/Offers.Domain/AggregationModels/Offer/Category/CategoryAggregate.cs
@@ -6,5 +6,8 @@ public class CategoryAggregate: BaseEntity
 {
     public string Name { get; protected set; }
 
-    protected CategoryAggregate() {}
+    public CategoryAggregate(string name)
+    {
+        Name = name;
+    }
 }
--- src/Services/Offers/Offers.Domain/AggregationModels/Offer/OfferAggregateRoot.cs
+++ src/Services/Offers/Offers.Domain/AggregationModels/Offer/OfferAggregateRoot.cs
@@ -7,7 +7,7 @@ namespace Offers.Domain.AggregationModels.Offer;
 
 public class OfferAggregateRoot: BaseEntityUnique
 {
-    public string AuthorId { get; protected set; }
+    public Guid AuthorId { get; protected set; }
     public string Title { get; protected set; }
     public string Content { get; protected set; }
     public DateTime CreatedAt { get; protected set; }
@@ -15,12 +15,26 @@ public class OfferAggregateRoot: BaseEntityUnique
     public bool IsArchival { get; protected set; }
 
     public int CategoryId { get; protected set; }
-    public virtual CategoryAggregate Category { get; protected set; }
+    public CategoryAggregate Category { get; protected set; }
     
     public int LocationId { get; protected set; }
-    public virtual LocationAggregate Location { get; protected set; }
+    public LocationAggregate Location { get; protected set; }
 
     public IList<ImageAggregate> Images { get; protected set; }
 
-    protected OfferAggregateRoot() {}
+    public OfferAggregateRoot(Guid authorId, string title, string content, DateTime createdAt, DateTime? updatedAt, 
+    bool isArchival, CategoryAggregate category, int locationId, LocationAggregate location, IList<ImageAggregate> images)
+    {
+        AuthorId = authorId;
+        Title = string.IsNullOrWhiteSpace(title) ? throw new ArgumentException(nameof(title)) : title;
+        Content = string.IsNullOrWhiteSpace(content) ? throw new ArgumentException(nameof(content)): content;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt?.Date;
+        IsArchival = isArchival;
+        Category = category ?? throw new ArgumentException(nameof(category));
+        CategoryId = category.Id;
+        Location = location ?? throw new ArgumentException(nameof(location));
+        LocationId = locationId;
+        Images = images ?? throw new ArgumentException(nameof(images));
+    }
 }
